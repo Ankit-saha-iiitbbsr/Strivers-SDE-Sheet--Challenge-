58ce76d0082490b0d72c7c417226cddfeba14736
Ankit Saha <120235923+Ankit-saha-iiitbbsr@users.noreply.github.com>
2023-06-04 12:19:18 +0530

    Create Pascal'sTriangle.cpp
    2. Pascal's Triangle

---
+++ Pascal'sTriangle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> generateRow(int row) {
+    long long ans = 1;
+    vector<long long> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
+
+    //calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+vector<vector<long long>> printPascal(int N) {
+    vector<vector<long long>> ans;
+
+    //store the entire pascal's triangle:
+    for (int row = 1; row <= N; row++) {
+        ans.push_back(generateRow(row));
+    }
+    return ans;
+}
